5baf36f83c4bc48aece1ad2450409fa55b67abcf
agussdiaz28 <agussdiaz28@gmail.com>
2018-06-08 13:38:32 -0300

    limpieza de botones de comanda y mejoras a pantalla de productos

--- gestion/views/producto/_form.php
+++ gestion/views/producto/_form.php
@@ -5,6 +5,7 @@ use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use kartik\select2\Select2;
 use app\models\Categoria;
+use app\models\Unidad;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Producto */
@@ -13,39 +14,50 @@ use app\models\Categoria;
 
 <div class="producto-form">
 
-      <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(); ?>
     
         <?= $form->field($model, 'codigo')->textInput(['maxlength' => true]) ?>
         <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+    
+    <?php
+        $unidades = Unidad::find()->all();
+        $listData = ArrayHelper::map($unidades,'id', 'nombre_unidad');
+        echo $form->field ($model, 'unidad_id', [
+            'template' => "{label} {input} {hint} {error}"])->widget(select2::classname(),
+            [
+                'data' => $listData ,
+                'language' => 'es',
+                'options' => ['placeholder' => 'Seleccione la unidad por defecto...'],
+                'pluginOptions' => [
+                    'allowClear' => false
+                ]
+            ]);
+    ?>
 
         <?php
          $categorias = Categoria::find()->all();
          $listData = ArrayHelper::map($categorias,'id', 'nombre');
 
-        echo $form->field ($model, 'categoria_id', ['template' => "{label} {input} {hint} {error}"]
-                          )->widget(select2::classname(), [
-                                                            'data' => $listData ,
-                                                            'language' => 'es',
-                                                            'options' => ['placeholder' => 'Seleccione una Categoria...'],
-                                                            'pluginOptions' => [
-                                                                'allowClear' => false
-                                                            ]
-                                                          ]);
-
+        echo $form->field ($model, 'categoria_id', [
+            'template' => "{label} {input} {hint} {error}"])->widget(select2::classname(),
+            [
+                'data' => $listData ,
+                'language' => 'es',
+                'options' => ['placeholder' => 'Seleccione una Categoria...'],
+                'pluginOptions' => [
+                    'allowClear' => false
+                ]
+            ]);
         ?>
 
         <?= $form->field($model, 'precio_unitario')->textInput(['type' => 'real']) ?>
 
       <div class="form-group" style="float:right;">
           <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-          <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
       </div>
 
 
 
 </div>
-
-
     <?php ActiveForm::end(); ?>
-
 </div>
