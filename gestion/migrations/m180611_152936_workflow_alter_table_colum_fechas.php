ea07f5faee2cd3e3a9050bd145ba6379121ce676
root <franco.e.aller@gmail.com>
2018-06-11 17:35:22 +0000

    m180611_152936_workflow_alter_table_colum_fechas cambio el tipo de columna a datetime

--- gestion/migrations/m180611_152936_workflow_alter_table_colum_fechas.php
+++ gestion/migrations/m180611_152936_workflow_alter_table_colum_fechas.php
@@ -12,9 +12,15 @@ class m180611_152936_workflow_alter_table_colum_fechas extends Migration
      */
     public function safeUp()
     {
-        $this->alterColumn("workflow",
-            "fecha_inicio",
-            $this->datetime()->notNull());
+        $this->alterColumn(
+         "workflow",
+        "fecha_inicio",
+                $this->datetime()->notNull());
+        
+        $this->alterColumn(
+          "workflow",
+         "fecha_fin",
+                $this->datetime()->null());
     }
 
     /**
@@ -23,6 +29,7 @@ class m180611_152936_workflow_alter_table_colum_fechas extends Migration
     public function safeDown()
     {
         $this->alterColumn("workflow","fecha_inicio",$this->date()->notNull());
+        $this->alterColumn("workflow","fecha_fin",$this->date()->null());
     
     }
 
