3e23a30ed2fb6f002d644d4c0803cbc4b5966abf
root <franco.e.aller@gmail.com>
2018-06-12 18:06:09 +0000

    filtro botones en grillas

--- gestion/models/PedidoSearch.php
+++ gestion/models/PedidoSearch.php
@@ -175,11 +175,13 @@ class PedidoSearch extends Pedido
             ,pedido.gestor_id
             ,pedido.estado_id as pedido_estado_id
             ,user.username
+            ,estado.id as estado_id
         ";
         $fromTables = '
             pedido
-            JOIN cliente                      ON(pedido.cliente_id=cliente.id)
-            JOIN user                      ON(pedido.gestor_id=user.id)
+            JOIN cliente                     ON(pedido.cliente_id=cliente.id)
+            JOIN user                        ON(pedido.gestor_id=user.id)
+            JOIN estado                      ON(pedido.estado_id=estado.id)
         ';
         
         
@@ -268,11 +270,13 @@ class PedidoSearch extends Pedido
             ,pedido.gestor_id
             ,pedido.estado_id as pedido_estado_id
             ,user.username
+            ,estado.id as estado_id
         ";
         $fromTables = '
             pedido
             JOIN cliente                      ON(pedido.cliente_id=cliente.id)
-            JOIN user                      ON(pedido.gestor_id=user.id)
+            JOIN user                         ON(pedido.gestor_id=user.id)
+            JOIN estado                       ON(pedido.estado_id=estado.id)
         ';
         
         
@@ -363,11 +367,13 @@ class PedidoSearch extends Pedido
             ,pedido.gestor_id
             ,pedido.estado_id as pedido_estado_id
             ,user.username
+            ,estado.id as estado_id
         ";
         $fromTables = '
             pedido
             JOIN cliente                      ON(pedido.cliente_id=cliente.id)
-            JOIN user                      ON(pedido.gestor_id=user.id)
+            JOIN user                         ON(pedido.gestor_id=user.id)
+             JOIN estado                      ON(pedido.estado_id=estado.id)
         ';
         
         
@@ -457,11 +463,13 @@ class PedidoSearch extends Pedido
             ,pedido.gestor_id
             ,pedido.estado_id as pedido_estado_id
             ,user.username
+            ,estado.id as estado_id
         ";
         $fromTables = '
             pedido
             JOIN cliente                      ON(pedido.cliente_id=cliente.id)
-            JOIN user                      ON(pedido.gestor_id=user.id)
+            JOIN user                         ON(pedido.gestor_id=user.id)
+             JOIN estado                      ON(pedido.estado_id=estado.id)
         ';
         
         
@@ -552,11 +560,13 @@ class PedidoSearch extends Pedido
             ,pedido.gestor_id
             ,pedido.estado_id as pedido_estado_id
             ,user.username
+            ,estado.id as estado_id
         ";
         $fromTables = '
             pedido
             JOIN cliente                      ON(pedido.cliente_id=cliente.id)
-            JOIN user                      ON(pedido.gestor_id=user.id)
+            JOIN user                         ON(pedido.gestor_id=user.id)
+             JOIN estado                      ON(pedido.estado_id=estado.id)
         ';
         
         
