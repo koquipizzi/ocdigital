d74f76c0c070a70513718f04145d1dd935e100ea
root <franco.e.aller@gmail.com>
2018-06-12 11:54:00 +0000

    proximo estado inserts, agrego la referencia a si mismo

--- gestion/migrations/m180611_214536_estadoProximo_inserts.php
+++ gestion/migrations/m180611_214536_estadoProximo_inserts.php
@@ -13,12 +13,16 @@ class m180611_214536_estadoProximo_inserts extends Migration
     public function safeUp()
     {
         $this->delete("estado_proximo","id>0");
+        $this->insert("estado_proximo",["estado_origen_id"=>1,"estado_destino_id"=>1]);
         $this->insert("estado_proximo",["estado_origen_id"=>1,"estado_destino_id"=>2]);
         $this->insert("estado_proximo",["estado_origen_id"=>1,"estado_destino_id"=>5]);
+        $this->insert("estado_proximo",["estado_origen_id"=>2,"estado_destino_id"=>2]);
         $this->insert("estado_proximo",["estado_origen_id"=>2,"estado_destino_id"=>3]);
         $this->insert("estado_proximo",["estado_origen_id"=>2,"estado_destino_id"=>5]);
+        $this->insert("estado_proximo",["estado_origen_id"=>3,"estado_destino_id"=>3]);
         $this->insert("estado_proximo",["estado_origen_id"=>3,"estado_destino_id"=>4]);
         $this->insert("estado_proximo",["estado_origen_id"=>3,"estado_destino_id"=>5]);
+        $this->insert("estado_proximo",["estado_origen_id"=>4,"estado_destino_id"=>4]);
         $this->insert("estado_proximo",["estado_origen_id"=>4,"estado_destino_id"=>5]);
         $this->insert("estado_proximo",["estado_origen_id"=>5,"estado_destino_id"=>1]);
         
