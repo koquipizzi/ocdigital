3b21c98e9d183a924a0363ff44387b1b3dae4b8e
root <franco.e.aller@gmail.com>
2018-06-12 12:53:18 +0000

    fix estado en index pedidos; al crear se asigna el stado 1 por defecto

--- gestion/views/pedido/index.php
+++ gestion/views/pedido/index.php
@@ -151,7 +151,7 @@ if(!empty($info))
                       'confirm' => function ($url, $model) {
                         if (Yii::$app->user->getId() == 7)
                             {
-                                $url =  Url::toRoute(['pedido/update', 'id' => $model["id"], 'proceso' => 'aceptar', "estado_origen_id"=>$model["estado_id"]]);
+                                $url =  Url::toRoute(['pedido/update', 'id' => $model["id"], 'proceso' => 'aceptar']);
                                 return Html::a('<span class="fa fa-check"></span>',Url::to($url));
                             }
                         else      
