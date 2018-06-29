dd5c3559d12399f0d198492b37faa61c50990de7
Alejandra Pizzi <alejandra@qwavee.com>
2018-06-29 17:03:41 -0300

    migration unidad

--- gestion/migrations/m180612_122130_pedido_add_column_estado_id.php
+++ gestion/migrations/m180612_122130_pedido_add_column_estado_id.php
@@ -14,11 +14,11 @@ class m180612_122130_pedido_add_column_estado_id extends Migration
     {
         $this->addColumn("pedido","estado_id",$this->integer(11)->defaultValue(1));
         
-        $this->createIndex(
-         'idx_pedido_estado-estado_id',
-         'pedido',
-         'estado_id'
-        );
+     //   $this->createIndex(
+    ///     'idx_pedido_estado-estado_id',
+    //     'pedido',
+     //    'estado_id'
+    //    );
     
         $this->addForeignKey(
          'idx_pedido_estado-estado_id',
@@ -40,10 +40,10 @@ class m180612_122130_pedido_add_column_estado_id extends Migration
          'pedido'
         );
     
-        $this->dropIndex(
-         'idx_pedido_estado-estado_id',
-         'pedido'
-        );
+    //    $this->dropIndex(
+     //    'idx_pedido_estado-estado_id',
+     //    'pedido'
+    //    );
         
         $this->dropColumn("pedido","estado_id");
     }
